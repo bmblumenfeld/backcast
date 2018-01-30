ee74f2578f24c98c29f2f1c92370226cb486ddb5
bmblumenfeld <bmblumenfeld@gmail.com>
2018-01-29 22:02:45 -0800

    lol

--- src/views/app.js
+++ src/views/app.js
@@ -3,12 +3,28 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);
+
+    this.searchView = new SearchView();
+    
+    this.videoListView = new VideoListView({collection: this.videos});
+    
+    this.videoPlayerView = new VideoPlayerView();
+    
+    this.render();
   },
 
 
   render: function() {
-    this.$el.html(this.template());
+    this.$el.html(this.template({
+      'searchBar': this.searchView.$el.html(),
+      'videoList': this.videoListView.$el.html(),
+      'videoPlayer': this.videoPlayerView.$el.html()
+      
+    }));
+    
+    
+        
     return this;
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,12 +1,23 @@
 var VideoListView = Backbone.View.extend({
 
-
-  render: function() {
-    this.$el.children().detach();
-    this.$el.html(this.template());
-    return this;
+  initialize: function() {
+    this.render();
   },
 
+  render: function() {
+    this.$el.children().detach();    
+    this.$el.html(this.template());
+    
+    var $videos = this.$el.find('.video-list');
+
+    this.collection.forEach(function (video) {
+      var videoView = new VideoListEntryView(video);            
+      $videos.append('<div>' + videoView.$el.html() + '</div>');
+    });
+    
+    return this;
+  },
+  
   template: templateURL('src/templates/videoList.html')
 
 });
